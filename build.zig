6a12d5c9fc9e329321c42aeedd6c77dc16753b63
Chris <chrisbreuer93@gmail.com>
2026-07-14 13:11:04 -0700

    ci: link libc in collector tests (#16)

--- build.zig
+++ build.zig
@@ -21,6 +21,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
             .sanitize_thread = tsan,
         }),
     });
