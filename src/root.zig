5b4a5b25f622c6346814433c30fc88efd52c4d3a
Chris <chrisbreuer93@gmail.com>
2026-07-14 14:34:29 -0700

    perf(gc): batch sweep backing releases (#17)

--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,7 @@
 
 pub const Heap = @import("heap.zig").Heap;
 pub const InteriorOwnership = @import("heap.zig").InteriorOwnership;
+pub const CellAllocation = @import("heap.zig").CellAllocation;
 
 test {
     _ = @import("heap.zig");
